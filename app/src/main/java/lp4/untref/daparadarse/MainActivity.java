c964d1af669d228f2590cbbdf5d32e9e8e133f6f
AlcidesMereles <dante1587@gmail.com>
2015-05-10 22:22:46 -0300

    Revert "Codigo de prueba que trae los datos"

--- app/src/main/java/lp4/untref/daparadarse/MainActivity.java
+++ app/src/main/java/lp4/untref/daparadarse/MainActivity.java
@@ -29,6 +29,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void listadoOnClick(View view){
+        startActivity(new Intent(this, ListadoActivity.class));
+
+
+    }
 
     public void EnviarOnClik(View view) {
         Thread nt = new Thread() {
@@ -63,11 +68,6 @@ public class MainActivity extends Activity {
             }
         };
         nt.start();
-    }
-
-    public void listadoOnClick(View view){
-        Intent intent = new Intent(MainActivity.this, ListadoActivity.class);
-        startActivity(intent);
     }
 
     public String enviarPost(String nombre, String apellido, String edad) {
