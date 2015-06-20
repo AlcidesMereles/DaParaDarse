a8c3af6baba1418a4e28a117d177580a6260af97
Matias <matytaa@gmail.com>
2015-06-19 22:48:55 -0300

    mostrar coincidencias

--- app/src/main/java/lp4/untref/daparadarse/Fragment_Chats.java
+++ app/src/main/java/lp4/untref/daparadarse/Fragment_Chats.java
@@ -1,6 +1,7 @@
 package lp4.untref.daparadarse;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -30,16 +31,40 @@ public class Fragment_Chats extends Fragment {
             throw new ClassCastException(activity.toString()
                     + " debe implementar OnHeadlineSelectedListener");
         }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fm_chats, container, false);
+        Button boton = (Button) rootView.findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    /* si estoy conectado muestro el boton de reserva online */
+                Intent miIntent = new Intent(getActivity(), ActivityCoincidencias.class);
+                startActivity(miIntent);
+
+            }
+        });
         return rootView;
     }
 
     public interface OnHeadlineSelectedListener {
         void onArticleSelected(int position);
+    }
+
+    public class Listener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), ActivityCoincidencias.class);
+            startActivity(intent);
+        }
     }
 
 
