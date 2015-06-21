78255c17772472bad57f8ee9d3a3e395e3657d66
Matias <matytaa@gmail.com>
2015-06-20 22:44:55 -0300

    mostrar perfil

--- app/src/main/java/lp4/untref/daparadarse/ClienteHttp.java
+++ app/src/main/java/lp4/untref/daparadarse/ClienteHttp.java
@@ -1,6 +1,5 @@
 package lp4.untref.daparadarse;
 
-import android.os.AsyncTask;
 import android.util.Log;
 
 import org.apache.http.HttpEntity;
@@ -16,22 +15,22 @@ import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
-import javaphpmysql.JSONObject;
-import javaphpmysql.JSONArray;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javaphpmysql.JSONArray;
+import javaphpmysql.JSONObject;
+
 public class ClienteHttp {
-    private boolean existeId;
+    private boolean existeId = false;
     private static final String SERVER_PATH = "http://daparadarse.site88.net/";
 
     public String enviarPost(Map<String, String> map) {
         HttpClient httpClient = new DefaultHttpClient();
         HttpContext localContext = new BasicHttpContext();
         HttpPost httpPost = new HttpPost(
-                "http://www.daparadarse.site88.net/Android/PutData.php");
+                "http://www.daparadarse.site88.net/Android/PutData.php?id=" + map.get("facebookID"));
         HttpResponse response = null;
         try {
             List<NameValuePair> params = new ArrayList<NameValuePair>();
@@ -59,7 +58,7 @@ public class ClienteHttp {
     public String leer(String idUsuarioFacebook) {
         HttpClient cliente = new DefaultHttpClient();
         HttpContext contexto = new BasicHttpContext();
-        HttpGet httpget = new HttpGet(SERVER_PATH + "/Android/pruebaCriterio.php?id=" + idUsuarioFacebook);
+        HttpGet httpget = new HttpGet(SERVER_PATH + "Android/pc_no_modificar.php?id=" + idUsuarioFacebook);
         String resultado = null;
         try {
             HttpResponse response = cliente.execute(httpget, contexto);
@@ -93,36 +92,48 @@ public class ClienteHttp {
         return listado;
     }
 
-    public boolean existeId(String id) {
+    public JSONArray obtenerJSON(String idUsuarioFacebook) {
+        String res = leer(idUsuarioFacebook);
+        JSONObject jObject;
+        JSONArray jArray = null;
 
-        new ComprobarExistenciaId().execute(id);
-
-        return true;
+        if (res != null) {
+            Log.i("res: ", res);
+            jObject = new JSONObject(res);
+            jArray = jObject.getJSONArray("usuarios");
+        } else {
+            Log.i("res nulo", "res nulo");
+        }
+        return jArray;
     }
 
-    private class ComprobarExistenciaId extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            HttpClient cliente = new DefaultHttpClient();
-            HttpContext contexto = new BasicHttpContext();
-            HttpGet httpget = new HttpGet(SERVER_PATH + "/Android/comprobarExistenciaId.php?id=" + params[0]);
-            String resultado = null;
-            try {
-                HttpResponse response = cliente.execute(httpget, contexto);
-                HttpEntity entity = response.getEntity();
-                resultado = EntityUtils.toString(entity, "UTF-8");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Log.i("Resulado de si existe", resultado);
-            return resultado;
+    public String enviarGustos(Map<String, String> map) {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+        HttpPost httpPost = new HttpPost(
+                "http://daparadarse.site88.net/Android/guardarGustos.php");
+        HttpResponse response = null;
+        try {
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("id", map.get("id")));
+            params.add(new BasicNameValuePair("otroId", map.get("otroId")));
+            params.add(new BasicNameValuePair("nombre", map.get("nombre")));
+//            params.add(new BasicNameValuePair("apellido", map.get("apellido")));
+//            params.add(new BasicNameValuePair("edad", map.get("edad")));
+//            params.add(new BasicNameValuePair("sexo", map.get("sexo")));
+//            params.add(new BasicNameValuePair("pais", map.get("pais")));
+//            params.add(new BasicNameValuePair("mujeres", map.get("mujeres")));
+//            params.add(new BasicNameValuePair("hombres", map.get("hombres")));
+//            params.add(new BasicNameValuePair("edadDesde", map.get("rangoDeEdadDesde")));
+//            params.add(new BasicNameValuePair("edadHasta", map.get("rangoDeEdadHasta")));
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            response = httpClient.execute(httpPost, localContext);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        @Override
-        protected void onPostExecute(String resultado) {
-            existeId = !resultado.equals("0");
-        }
+        System.out.println(response.toString());
+        return response.toString();
     }
+
+
 }
