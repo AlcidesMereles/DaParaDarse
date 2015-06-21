e3eeb11c3ff726defc4cc5b9a066315fc2d65652
Matias <matytaa@gmail.com>
2015-06-20 22:49:54 -0300

    mostrar perfil

--- app/src/main/java/lp4/untref/daparadarse/MainActivity.java
+++ app/src/main/java/lp4/untref/daparadarse/MainActivity.java
@@ -10,11 +10,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.ViewGroup;
 
-/**
- * Created by Spider on 07/06/2015.
- */
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener, ViewPager.OnPageChangeListener, Fragment_Chats.OnHeadlineSelectedListener {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener, ViewPager.OnPageChangeListener, Fragment_Chats.OnHeadlineSelectedListener{//, LoginFragment.OnSetIdListener {
     private ViewPager mViewPager;
+
+    FragmentManager fm;
+    public Fragment miFragment;
+    String facebookID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,8 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
         mViewPager.setOnPageChangeListener(this);
 
-        ActionBar actionBar = getSupportActionBar();
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         ActionBar.Tab tab = actionBar.newTab().setText("Perfil").setTabListener(this);
@@ -38,6 +41,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
         tab = actionBar.newTab().setText("Chats").setTabListener(this);
         actionBar.addTab(tab);
+
     }
 
 
@@ -84,10 +88,10 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
             super(fm);
         }
 
+
         public Fragment getItem(int arg0) {
             switch (arg0) {
                 case 0:
-
                     return new LoginFragment();
                 case 1:
                     return new Fragment_Elegir();
@@ -101,9 +105,31 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         public int getCount() {
             return 3;
         }
+        //TODO: Probar y borrar si no se usa
+        @Override
+        public int getItemPosition(Object object) {
+            if (object instanceof Fragment_Elegir &&
+                    miFragment instanceof Fragment_Elegir) {
+                return POSITION_NONE;
+            }
+            return POSITION_UNCHANGED;
+        }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
         }
+    }
+/**
+    @Override
+    public void OnSetId(String userID) {
+
+        facebookID = userID;
+        Fragment_Elegir fe;
+        fe = (Fragment_Elegir) getSupportFragmentManager().getFragments().get(1);
+        fe.OnSetId(facebookID);
+    }
+**/
+    public String getFacebookID() {
+        return facebookID;
     }
 }
